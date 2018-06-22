d68213d0eba0a09a22852bedb625f160f6a53980
Zhouwy <zzzwwwyyy@foxmail.com>
2018-06-22 18:10:09 +0800

    last commit of jdah

--- jdah/StudyC.c
+++ jdah/StudyC.c
@@ -363,6 +363,10 @@ struct mutableArray *pDta = malloc(sizeof(struct mutableArray) + 1024); //这是
 memcpy(pDta->data, "Hello Mutable Array...", 1024);
 
 
+memmove用于从src拷贝count个字节到dest，如果目标区域和源区域有重叠的话，
+memmove能够保证源串在被覆盖之前将重叠区域的字节拷贝到目标区域中。
+但复制后src内容会被更改。但是当目标区域与源区域没有重叠则和memcpy函数功能相同.
+
 
 
 
